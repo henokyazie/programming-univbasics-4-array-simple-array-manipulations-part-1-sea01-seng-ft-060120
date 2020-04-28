bef470cb2f0ae5b961edf9d973b4ce23cd0019d0
brave-ping-4201 <h_yazie@yahoo.com>
2020-04-28 09:14:52 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(array, string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  colors_in_the_rainbow.push("violet")
+end
+
+def using_unshift(array, string)
+   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+   bouroughs_in_nyc.unshift("Staten Island")
+end
+
+def using_pop(array)
+  @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  antarctica = @continents.pop
+  p antarctica
+end
+
+def pop_with_args(array)
+  @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  @dog_breeds.pop(2)
+end
+
+def using_shift(array)
+  @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  lagos = @my_favorite_cities.shift 
+  p @my_favorite_cities
+end
+  
